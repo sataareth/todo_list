c21734e4092fdc052e9c25c2edceb470e1be761b
sataareth <satareth@gmail.com>
2018-03-12 00:15:15 +0300

    Revert "Delete add.php"
    This reverts commit 1e67f04d37263bc3f258805d2184d72fe7caefc3.

---
+++ add.php
@@ -0,0 +1,11 @@
+﻿<?php
+    require_once("init.php");
+
+    if(isset($_POST['name'])) {
+      $name = $_POST['name'];
+      $result = "INSERT INTO tasks (name, done) VALUES ('$name', 0);";
+      $db->query($result);
+      header("Location: ../index.php?add=success");
+    }
+
+?>
